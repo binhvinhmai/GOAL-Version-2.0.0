f4f7e8855d46fc38ce2362d672d508e31fc8adea
Binh Mai <binhvinhmai@gmail.com>
2016-03-02 20:31:13 -0600

    Initial Commit

---
+++ GOAL Release 2.0.0Tests/GOAL_Release_2_0_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GOAL_Release_2_0_0Tests.swift
+//  GOAL Release 2.0.0Tests
+//
+//  Created by Binh Vinh Mai on 3/2/16.
+//  Copyright © 2016 Binh Vinh Mai. All rights reserved.
+//
+
+import XCTest
+@testable import GOAL_Release_2_0_0
+
+class GOAL_Release_2_0_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
